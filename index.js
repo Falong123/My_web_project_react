b20ec93b55a5d414b01970754f2a6d4c9a4c0c3d
Nguyễn Quốc Đạt <114865670+Falong123@users.noreply.github.com>
2025-10-30 00:28:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.js'; // Nhập khẩu component chính
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
